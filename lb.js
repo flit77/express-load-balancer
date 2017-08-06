fe5ca51f31c77f703ed38de2f987a8c5e5c73cb3
Vasiliy Shubko <flit77@gmail.com>
2017-08-06 22:37:13 +0200

    error hadnling in lb

--- lb.js
+++ lb.js
@@ -4,7 +4,10 @@ const servers = ['http://localhost:3000', 'http://localhost:3001'];
 let cur = 0;
 
 const handler = (req, res) => {
-  req.pipe(request({ url: servers[cur] + req.url })).pipe(res);
+  const _req = request({ url: servers[cur] + req.url }).on('error', error => {
+    res.status(500).send(error.message);
+  });
+  req.pipe(_req).pipe(res);
   cur = (cur + 1) % servers.length;
 };
 const server = express().get('*', handler).post('*', handler);
